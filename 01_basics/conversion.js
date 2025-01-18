2a5de4b0e6af392dc8e45776d013a4d2661e6755
bharateshantanu <163708246+bharateshantanu@users.noreply.github.com>
2025-01-18 12:51:09 +0000

    container config done

---
+++ 01_basics/conversion.js
@@ -0,0 +1,30 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valInNum = Number(score)
+console.log(valInNum)
+console.log(typeof(valInNum))
+
+// "33" => 33
+// "33abc "=> NaN but type is number
+// true =>1 false => 0
+
+// number to boolean
+
+let isLogged =1;
+let valNum = Boolean(isLogged)
+
+console.log(valNum)
+
+// 1 => true , 0 => false
+// ""=> false
+//"shantanu" => true
+
+//Converting to STrings
+
+let num =33;
+let strNum = String(num)
+
+console.log(strNum)
