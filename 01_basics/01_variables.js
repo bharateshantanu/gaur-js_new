e88c19ab21b77c8ae010619333fda576d602457f
bharateshantanu <163708246+bharateshantanu@users.noreply.github.com>
2025-01-18 12:37:11 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accId =123
+let accMail ="an"
+var accPass ="123344"
+accCity ="pune"
+
+//accId =2 // not allowed bcoz of const keyword
+
+accMail ="abc@gmail.com"
+accPass ="1123"
+accCity = "Mum"
+
+console.table([accMail,accId,accPass,accCity])
+
+/*
+try not to use var bcoz it creates problem in functional scope
+*/
---
+++ 01_basics/02_variables.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version of js
+
+//alert("hello")// we are using node.js and not browser
+
+let name = "Gaur" // string data type
+let age =17 
+let isLogged = true// boolean data type
+
+//number =>2 to power 53 range
+//bigInt greater than this no
+//string => use in double quotes
+//boolean => true or false
+// null =>it is actually standalone value
+// undefined => value is not assigned yet
+// symbol => used in react for uniqueness
+
